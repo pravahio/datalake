c6c739c140ac0f400888e6c11f82bdaae6ab29ef
Abhishek Upperwal <a.upperwal@gmail.com>
2019-12-25 14:55:59 +0530

    can fetch past data

--- datalake.py
+++ datalake.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timedelta
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class Datalake:
     def __init__(self, usr, pwd, channel):
@@ -7,16 +9,59 @@ class Datalake:
         
         channel = channel.replace('/', '').lower()
         print('Working on collection: ' + channel)
-        self.db = self.client.datalake[channel]
+        self.collection = self.client.datalake[channel]
 
     def insert(self, geospace, obj):
         data = {
             'geospace': geospace,
             'item': obj
         }
-        return self.db.insert(data)
+        return self.collection.insert(data)
     
-    def get(self, obj):
-        return self.db.find_one({
-            '_id': obj
-        })
+    # past: 60 mins
+    # start: 2019/12/23 00:00:00
+    def get(self, obj, start='', end='', past=60):
+        if isinstance(obj, ObjectId):
+            return self.collection.find_one({
+                '_id': obj
+            })
+        elif isinstance(obj, dict):
+            if 'geospace' in obj:
+                if start != '' and end != '':
+                    start_date = ObjectId.from_datetime(datetime.strptime(start, '%Y/%m/%d %H:%M:%S'))
+                    end_date = ObjectId.from_datetime(datetime.strptime(end, '%Y/%m/%d %H:%M:%S'))
+                    return self.collection.find({
+                        '_id': {
+                            '$gte': start_date,
+                            '$lte': end_date
+                        },
+                        'geospace': obj['geospace'],
+                    })
+                elif start != '':
+                    print('this')
+                    start_date = ObjectId.from_datetime(datetime.strptime(start, '%Y/%m/%d %H:%M:%S'))
+                    return self.collection.find({
+                        '_id': {
+                            '$gte': start_date
+                        },
+                        'geospace': obj['geospace'],
+                    })
+                elif end != '':
+                    end_date = ObjectId.from_datetime(datetime.strptime(end, '%Y/%m/%d %H:%M:%S'))
+                    return self.collection.find({
+                        '_id': {
+                            '$lte': end_date
+                        },
+                        'geospace': obj['geospace'],
+                    })
+                else:
+                    date = datetime.utcnow() - timedelta(minutes=past) 
+                    start_date = ObjectId.from_datetime(date)
+                    return self.collection.find({
+                        '_id': {
+                            '$gte': start_date
+                        },
+                        'geospace': obj['geospace'],
+                    })
+        raise Exception
+
