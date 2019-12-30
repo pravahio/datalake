6a05b82fd42172dd9b7b2650d00a79093d0e5d3d
Abhishek Upperwal <a.upperwal@gmail.com>
2019-12-30 16:44:43 +0530

    added query ex

--- fetch.py
+++ fetch.py
@@ -1,4 +1,5 @@
 import json
+import pytz
 
 from datalake import Datalake
 
@@ -10,6 +11,7 @@ def main():
 
     # Available channels and geospaces: https://github.com/pravahio/go-mesh/wiki/Geospaces
     # '/PublicBus', '/AirQuality', '/SolarPowerProduction'
+    # Geospaces would differ from channel to channel.
     datalake = Datalake('pravah', '', '/AirQuality')
 
     # All the data after 'start' datetime
@@ -19,14 +21,22 @@ def main():
     #cur = datalake.get({'geospace': '/in/ncr'}, start='2019/12/24 18:00:00')
 
     # data between 'start' and 'end' datetime
-    #cur = datalake.get({'geospace': '/in/ncr'}, start='2019/12/24 18:00:00', end='2019/12/25 18:00:00')
+    #cur = datalake.get({'geospace': '/in/ncr'}, start='2019/12/24 18:00:00', end='2019/12/24 21:00:00')
 
-    # data from the past 60 mins
-    cur = datalake.get({'geospace': '/in/ncr'}, past=120)
+    # data from the past 120 mins
+    #cur = datalake.get({'geospace': '/in/ncr'}, past=120)
+
+    # query your data
+    cur = datalake.get({
+        "item.stations.id": "site_5050"
+        }, 
+        past=20 # mins
+    )
 
     print('Total Results: ' + str(cur.count()))
+    ist = pytz.timezone('Asia/Kolkata')
     for c in cur:
-        print(c['_id'].generation_time)
+        print(c['_id'].generation_time.astimezone(ist))
 
 if '__main__' == __name__:
     main()
