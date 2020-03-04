ee9a7c163f5f04265f023a9a47652dd373edb07c
Abhishek Upperwal <a.upperwal@gmail.com>
2020-03-04 21:24:10 +0530

    auth added

--- setup.py
+++ setup.py
@@ -5,21 +5,25 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dlake",
-    version="0.0.5",
+    version="0.1.2",
     author="Abhishek Upperwal",
     author_email="mesh@soket.in",
     description="Dynamic timer to learn the update frequency of a resource on the web",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/upperwal",
+    package_dir={'dlake':'dlake'},
     packages=setuptools.find_packages(),
-    package_dir={'':'src'},
     py_modules=['dlake'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    package_data={
+        'dlake': ['*.crt'],
+    },
+    include_package_data=True,
     install_requires = [
         'pymongo'
     ]
